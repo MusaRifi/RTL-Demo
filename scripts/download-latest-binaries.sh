932238dc941a63d764d10f80313258557d3780da
neeraj31285 <neeraj.singh31285@gmail.com>
2026-01-16 12:29:20 +0530

    prebuilt bins renaming.

--- scripts/download-latest-binaries.sh
+++ scripts/download-latest-binaries.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Downloading latest RTL binaries (Linux/clang)..."
+echo "Downloading latest RTL binaries (Linux/Clang)..."
 
-mkdir -p prebuilt-bins
-cd prebuilt-bins
+mkdir -p rtl-release-clang
+cd rtl-release-clang
 
 BASE_URL="https://github.com/ReflectCxx/ReflectionTemplateLibrary-CPP/releases/download/latest"
 
 curl -L -o RTLTestRunApp-linux-clang \
-  "$BASE_URL/RTLTestRunApp-linux-clang"
+  "$BASE_URL/RTLTestRunApp-clang"
 
-curl -L -o RTLBenchmarkApp-linux-clang \
-  "$BASE_URL/RTLBenchmarkApp-linux-clang"
+curl -L -o RTLBenchmarkApp-clang \
+  "$BASE_URL/RTLBenchmarkApp-clang"
 
-chmod +x RTLTestRunApp-linux-clang RTLBenchmarkApp-linux-clang
+# Rename to clean names
+mv RTLTestRunApp-clang RTLTestRunApp
+mv RTLBenchmarkApp-clang RTLBenchmarkApp
 
-echo "Done."
+# Make executable
+chmod +x RTLTestRunApp RTLBenchmarkApp
+
+echo "Done downloading clang-prebuilt Test & Benchmarking binaries."
